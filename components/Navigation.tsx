3b5d00331179afea4ea310cdace584755ce913af
unknown <nativzgo@gmail.com>
2021-08-17 22:33:11 +0200

    created navigation

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,5 +1,24 @@
+import { Flex, Box, Spacer } from '@chakra-ui/layout';
+import { Button } from '@chakra-ui/button';
+import Link from 'next/link';
 const Navigation = () => {
-  return <div>Navigation</div>;
+  return (
+    <Flex py="4" px="96" backgroundColor="blue.400" align="center">
+      <Box></Box>
+      <Spacer />
+      <Box>
+        <Button colorScheme="gray" mr="6">
+          <Link href="/">Home</Link>
+        </Button>
+        <Button colorScheme="gray" mr="6">
+          <Link href="/projects">Projects</Link>
+        </Button>
+        <Button colorScheme="gray">
+          <Link href="/contact">Contact</Link>
+        </Button>
+      </Box>
+    </Flex>
+  );
 };
 
 export default Navigation;
---
+++ theme.ts
@@ -0,0 +1,13 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  colors: {
+    transparent: 'transparent',
+    black: '#000',
+    white: '#fff',
+    blue: {
+      50: '#EBF8FF',
+      400: '#4299E1',
+      200: '#90CDF4',
+    },
+  },
+};
