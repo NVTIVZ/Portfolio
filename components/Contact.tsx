940be57d976f589e4d51a7521eab8f0dc6878aff
unknown <nativzgo@gmail.com>
2022-01-05 10:32:50 +0100

    update links

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -100,7 +100,7 @@ const Contact = () => {
           </Button>
         </Link>
         <Link
-          href="https://drive.google.com/file/d/1U_0Psk3U1uKVRosoRc6B_8e78UfZVPmo/view?usp=sharing"
+          href="https://drive.google.com/file/d/1E7Y0J2GYNe8qNS0sfbMZETnaFXDfZQp_/view?usp=sharing"
           target="_blank"
         >
           <Button
